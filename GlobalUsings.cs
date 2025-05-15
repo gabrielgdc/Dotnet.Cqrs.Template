922c4eb21f1484299327684c373cbcad8e8a5093
Gabriel Guedes <gabrielgdc15@gmail.com>
2025-05-15 08:15:23 -0300

    chore: removing unused files

--- GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using System.Data.Common;
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.Extensions.Options;
-global using System.Threading.Tasks;
-global using System;
